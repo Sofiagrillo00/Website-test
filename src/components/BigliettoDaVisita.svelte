aa4bf714efcd46e5b0bc6271ed0bf0815d6850c3
Sofiagrillo00 <sofia.grillo@unirufa.it>
2024-03-07 16:48:08 +0100

    Primo aggiornamento

---
+++ src/components/BigliettoDaVisita.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let nome;
+	export let indirizzo;
+	export let mail;
+	export let numero;
+
+	export let image;
+</script>
+
+<div class="Biglietto">
+	<img class="Biglietto_immagine" src={image} alt="copertina" />
+
+	<div class="BigliettoDaVisita_Top">
+		<p class="BigliettoDaVisita">{nome}</p>
+	</div>
+	<div class="BigliettoDaVisita_Centro">
+		<p class="BigliettoDaVisita">{indirizzo}</p>
+		<p class="BigliettoDaVisita">{mail}</p>
+		<p class="BigliettoDaVisita">{numero}</p>
+	</div>
+</div>
+
+<style>
+	.Biglietto {
+		background-color: rgb(253, 247, 235);
+		width: 600px;
+		border-radius: 20px;
+		padding: 20px;
+		font-family: sans-serif;
+	}
+
+	.BigliettoDaVisita {
+		font-size: 25px;
+		font-family: serif;
+	}
+
+	.BigliettoDaVisita_Top {
+		margin-top: 30px;
+		margin-bottom: 40px;
+	}
+
+	.BigliettoDaVisita_Centro {
+		margin-top: 15px;
+		margin-bottom: 15px;
+	}
+
+	.Biglietto_immagine {
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
-<script lang="ts">
-	import { get_collection } from '$cms';
+<script>
+	import BigliettoDaVisita from '$components/BigliettoDaVisita.svelte';
+
+	let Biglietto = {
+		nome: 'Sofia Grillo',
+		indirizzo: 'Piazza della trivulziana 2',
+		mail: 'sofiagrillo0@gmail.com',
+		numero: '3313447376',
+		image:
+			'https://images.pexels.com/photos/18031818/pexels-photo-18031818/free-photo-of-mano-estate-fiore-tenendo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+	};
+
+	let Biglietti = [
+		{
+			nome: 'Sara',
+			indirizzo: 'Ciao',
+			mail: '10:20',
+			numero: '1230',
+			image:
+				'https://images.pexels.com/photos/20264866/pexels-photo-20264866/free-photo-of-relax-letto-biondo-sorridente.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+		}
+	];
 </script>
 
-Home page
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
+</svelte:head>
+
+<BigliettoDaVisita
+	nome="Sofia Grillo"
+	indirizzo="Piazza della trivulziana 2"
+	mail="sofiagrillo0@gmail.com"
+	numero="3313447376"
+	image="https://images.pexels.com/photos/18031818/pexels-photo-18031818/free-photo-of-mano-estate-fiore-tenendo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+></BigliettoDaVisita>
